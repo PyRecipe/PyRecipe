8fe044f626fea81cca79b83852825e765a17b201
Eren Kaymakcı <61030413+ErenKaymakci@users.noreply.github.com>
2021-05-10 21:41:47 +0300

    Created recipe model (#24)

--- app/models.py
+++ app/models.py
@@ -12,7 +12,17 @@ class User(models.Model):
 class Comment(models.Model):
     recipe_id = models.CharField(max_length=30)
     user_id = models.CharField(max_length=30)
-    comment = models.CharField(max_length=500)
+    comment = models.TextField(max_length=500)
     created_at = models.DateTimeField()
     
+class Recipe(models.Model):
+    slug = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    images = models.ImageField() # vermedim ancak parametre olarak belirli width,height ve max length değeri verebiliriz.
+    components = models.TextField(max_length=500)
+    state = models.IntegerField() 
+    category = models.CharField(max_length=50)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
 
