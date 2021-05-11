8ee4315848f4bdc1945c387a5f935ecbf4f3cca0
Berkay Çubuk <berkaycubuk@gmail.com>
2021-05-12 01:12:02 +0300

    Settings page created (#29)
    Settings page created and connected

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'app'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('ayarlar', views.settings, name='settings'),
 ]
