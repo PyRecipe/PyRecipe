9ec73e1de4dad27757b14e59d6e204d822398d44
Berkay Çubuk <berkaycubuk@gmail.com>
2021-05-12 00:24:30 +0300

    Settings page created
    Settings page created and connected

--- app/views.py
+++ app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # homepage
 def index(request):
     return render(request, 'index.html')
+
+# settings
+def settings(request):
+    return render(request, 'settings.html')
