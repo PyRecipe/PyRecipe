c9b4b2d6c2efd5dec888d25117c01a6af7b19093
Berkay Çubuk <berkaycubuk@gmail.com>
2021-05-14 22:53:33 +0300

    Recipe model __str__ function added

--- app/models.py
+++ app/models.py
@@ -26,3 +26,7 @@ class Recipe(models.Model):
     category = models.CharField(max_length=50, null=True)
     created_at = models.DateTimeField(default=timezone.now())
     updated_at = models.DateTimeField(default=timezone.now())
+
+    # If object called without a parameter
+    def __str__(self):
+        return self.title
