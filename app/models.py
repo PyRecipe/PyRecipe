eec347847fb2fb165021bf8d68e78a284739afc0
Berkay Çubuk <berkaycubuk@gmail.com>
2021-05-15 00:15:23 +0300

    Dates are defaultly created in required fields

--- app/models.py
+++ app/models.py
@@ -6,15 +6,15 @@ class User(models.Model):
     fullname = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
-    confirmed_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
-    created_at = models.DateTimeField()
+    confirmed_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(default=timezone.now())
+    created_at = models.DateTimeField(default=timezone.now())
 
 class Comment(models.Model):
     recipe_id = models.CharField(max_length=30)
     user_id = models.CharField(max_length=30)
     comment = models.TextField(max_length=500)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(default=timezone.now())
     
 class Recipe(models.Model):
     slug = models.CharField(max_length=50)
