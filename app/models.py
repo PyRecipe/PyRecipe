e289476246e8e26e58adcc08b39a24c68f07648c
Berkay Çubuk <berkaycubuk@gmail.com>
2021-05-16 00:12:57 +0300

    Custom User model removed, default one is looks enough (#66)

--- app/models.py
+++ app/models.py
@@ -1,13 +1,5 @@
 from django.db import models
-
-class User(models.Model):
-    username = models.CharField(max_length=200)
-    fullname = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    confirmed_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
-    created_at = models.DateTimeField()
+from django.contrib.auth.models import User
 
 class Comment(models.Model):
     recipe_id = models.CharField(max_length=30)
