656b60223ef0803ee3c954a0957c4c168c88af7c
Berkay Çubuk <berkaycubuk@gmail.com>
2021-05-16 15:42:39 +0300

    Recipe page: images fixed

--- app/models.py
+++ app/models.py
@@ -1,18 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils import timezone
-
-class User(models.Model):
-    username = models.CharField(max_length=200)
-    fullname = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    confirmed_at = models.DateTimeField(null=True)
-    updated_at = models.DateTimeField(default=timezone.now())
-    created_at = models.DateTimeField(default=timezone.now())
-
-    # If object called without a parameter
-    def __str__(self):
-        return self.username
 
 class Comment(models.Model):
     recipe_id = models.CharField(max_length=30)
@@ -36,16 +24,20 @@ class Recipe(models.Model):
     def __str__(self):
         return self.title
 
+    def author_info(self):
+        """Returns information about the author"""
+        return User.objects.get(pk=self.author)
+
     def images_arr(self):
         """Returns images as an array"""
-        if self.images is not None:
+        if self.images is not None and self.images != "":
             return self.images.split(',')
         else:
             return []
 
     def components_arr(self):
         """Returns components as an array"""
-        if self.components is not None:
+        if self.components is not None and self.components != "":
             return self.components.split(',')
         else:
             return []
