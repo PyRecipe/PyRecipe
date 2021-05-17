e5e8d6c960e1d924b4df2da9c3038ab3d826e085
ErenKaymakci <erenkaymakcii@outlook.com>
2021-05-18 01:13:44 +0300

    Created Register Func

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from app.models import Recipe
+from django.contrib.auth.models import User
 import json
 
 class TestViews(TestCase):
@@ -14,6 +15,9 @@ class TestViews(TestCase):
         self.recipe_url = reverse('app:recipe', args=['recipe'])
         self.recipe_undefined_url = reverse('app:recipe', args=['this-recipe-not-exists'])
 
+        #  register setUp
+        self.register_url = reverse('app:register')
+
     def test_recipe_GET(self):
         response = self.client.get(self.recipe_url)
 
@@ -25,3 +29,36 @@ class TestViews(TestCase):
 
         # is it redirected
         self.assertEquals(response.status_code, 302)
+
+
+    """ Register Tests  """
+    
+    def test_register_GET(self):
+        response = self.client.get(self.register_url)
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response, 'register.html')
+    
+    def test_create_user(self):
+        amount_of_user = len(User.objects.all())
+        response = self.client.post(
+            self.register_url,
+            {
+                'first_name': 'Kevin',
+                'last_name' : 'Wmes',
+                'username' : 'example12321',
+                'email': 'example34@hotmail.com',
+                'password1' : 'Aa90019001',
+                'password2' : 'Aa90019001'
+            }
+        ) 
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(User.objects.all()),amount_of_user + 1)
+        
+
+
+
+
+
+        
+
