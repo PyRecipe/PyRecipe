7fc80716021700c56db6c3d217a8ebe1111990ea
Zekeriya AY <ay.zekeriya2000@gmail.com>
2021-05-20 18:32:50 +0300

    Update urls.py

--- PyRecipe/urls.py
+++ PyRecipe/urls.py
@@ -21,7 +21,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('app.urls', namespace='app')),
     path('admin/', admin.site.urls),
-]
-if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
